3e3ac1267f054af5fd9b11f0442d3a61d8e6be9f
Luismkm <luismarcelo13@yahoo.com.br>
2023-05-15 14:16:50 -0300

    refactor: remove unused code

--- src/components/template/BoasVindas.tsx
+++ src/components/template/BoasVindas.tsx
@@ -1,8 +1,6 @@
 // https://unicode-table.com/en/1F44B/
 import { useContext } from 'react'
 import AutenticacaoContext from '@/data/contexts/AutenticacaoContext'
-import usuario from '@/data/constants/usuarioFalso'
-
 export default function BoasVindas() {
     const { usuario } = useContext(AutenticacaoContext)
 
--- src/data/constants/usuarioFalso.ts
+++
@@ -1,9 +0,0 @@
-import Id from "@/logic/core/comum/Id";
-import Usuario from "@/logic/core/usuario/Usuario";
-
-export default {
-  id: Id.novo(),
-  nome: 'Luis',
-  email: 'luis@email.com',
-  imagemUrl: null,
-} as Usuario
