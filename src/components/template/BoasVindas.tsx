12eff8eecd0ad7d2a22b0cc39627e3515281cb57
Luismkm <luismarcelo13@yahoo.com.br>
2023-05-15 09:38:51 -0300

    finalize app

---
+++ src/components/template/BoasVindas.tsx
@@ -0,0 +1,18 @@
+// https://unicode-table.com/en/1F44B/
+import { useContext } from 'react'
+import AutenticacaoContext from '@/data/contexts/AutenticacaoContext'
+import usuario from '@/data/constants/usuarioFalso'
+
+export default function BoasVindas() {
+    const { usuario } = useContext(AutenticacaoContext)
+
+    return (
+        <div className={`text-3xl font-black`}>
+            Olá {' '}
+            <span className="hidden sm:inline">
+              {usuario?.nome?.split(' ')[0]}
+            </span>{' '}
+            👋
+        </div>
+    )
+}
---
+++ src/data/constants/usuarioFalso.ts
@@ -0,0 +1,9 @@
+import Id from "@/logic/core/comum/Id";
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+  id: Id.novo(),
+  nome: 'Luis',
+  email: 'luis@email.com',
+  imagemUrl: null,
+} as Usuario
